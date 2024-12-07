af6deee0d5be9b9d10252f1b38e514433221f766
Aditya <adi22feb@gmail.com>
2024-12-07 14:34:35 +0530

    Removed about and teams page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
-import { About } from "./components/about";
+// import { About } from "./components/about";
 import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
+// import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
@@ -26,15 +26,15 @@ const App = () => {
     <div>
       <Navigation />
       <Header data={landingPageData.Header} />
-      <About data={landingPageData.About} />
-      <Team data={landingPageData.Team} />
+      {/* <About data={landingPageData.About} /> */}
+      {/* <Team data={landingPageData.Team} /> */}
       <Features data={landingPageData.Features} />
       <Testimonials data={landingPageData.Testimonials} />
       {/* <Services data={landingPageData.Services} /> */}
       <Gallery data={landingPageData.Gallery} />
       <Contact data={landingPageData.Contact} />
       <a
-        href="https://www.google.com"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSf0AeYmQtT3xlpKXcaLIqdHmtxlZ-8UYkMd6AY8zriuSvpOrg/viewform"
         target="_blank"
         rel="noopener noreferrer"
         className="btn btn-custom btn-lg btn-enroll-now"
