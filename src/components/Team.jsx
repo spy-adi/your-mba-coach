50b571d085df4416f16841d0f64e47085b7bf702
Umang Tulsan <umangtulsan@Umangs-MacBook-Air.local>
2024-12-09 01:56:21 +0530

    Chnges

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -19,8 +19,9 @@ export const Team = (props) => {
   return (
     <div id="team" className="text-center">
       <div className="container">
-        <div className="col-md-8 col-md-offset-2 section-title">
+        <div id="after" className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
+
         </div>
         <div id="row">
           {props.data
@@ -32,7 +33,7 @@ export const Team = (props) => {
                     className="col-md-4 col-sm-6 team"
                   >
                     <div className="thumbnail">
-                      <img src={d.img} alt="..." className="team-img" />
+                      <img src={d.img} alt="........" className="team-img" />
                       <div className="caption">
                         <h4>{d.name}</h4>
                         <p>
@@ -42,7 +43,7 @@ export const Team = (props) => {
                               onClick={() => toggleExpand(i)}
                               className="btn-toggle"
                             >
-                              {isExpanded ? "show Less" : "more"}
+                              {isExpanded ? "show Less" : "...show more"}
                             </button>
                           )}
                         </p>
